a3fa5f8643d8e1d2dda8556417cd2de1aae3570e
Stalin Narváez <108230261+StalinNarvaez@users.noreply.github.com>
2023-04-17 14:21:12 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello World");
+  let listaPronombre = ["mi", "la", "el", "tu"];
+  let listaAdjetivo = ["gran", "genial", "impecable", "educativa"];
+  let listaSustantivo = ["escuela", "centro", "espacio", "empresa"];
+  let listaDominio = [".com", ".io", ".edu", ".org"];
+
+  for (let x in listaPronombre) {
+    for (let y in listaAdjetivo) {
+      for (let z in listaSustantivo) {
+        for (let i in listaDominio) {
+          console.log(
+            listaPronombre[x] +
+              listaAdjetivo[y] +
+              listaSustantivo[z] +
+              listaDominio[i]
+          );
+        }
+      }
+    }
+  }
 };
